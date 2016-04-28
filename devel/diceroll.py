69a4bc3520dcee696fa5acc6b87ea10b0a4369fc
Joshua Savon <jsavon@mail.valenciacollege.edu>
2016-04-27 21:26:10 -0400

    some changes
    loop

--- devel/diceroll.py
+++ devel/diceroll.py
@@ -1,5 +1,5 @@
 import random
-
+print("")
 print("Welcome to Josh's Dice rolling program!")
 print("Press enter to roll the die")
 input()
@@ -7,52 +7,108 @@ input()
 number=random.randint(1,6)
 
 if number==1:
-    print("You rolled a one!")
-    print("[----------]")
-    print("[          ]")
-    print("[     O    ]")
-    print("[          ]")
-    print("[----------]")
+   print("You rolled a one!")
+   print("[----------]")
+   print("[          ]")
+   print("[     O    ]")
+   print("[          ]")
+   print("[----------]")
 
 if number==2:
-    print("You rolled a two!")
-    print("[----------]")
-    print("[          ]")
-    print("[   O  O   ]")
-    print("[          ]")
-    print("[----------]")
+   print("You rolled a two!")
+   print("[----------]")
+   print("[          ]")
+   print("[   O  O   ]")
+   print("[          ]")
+   print("[----------]")
     
-
 if number==3:
-    print("You rolled a three!")
-    print("[----------]")
-    print("[          ]")
-    print("[    O     ]")
-    print("[   O O    ]")
-    print("[          ]")
-    print("[----------]")
+   print("You rolled a three!")
+   print("[----------]")
+   print("[          ]")
+   print("[    O     ]")
+   print("[   O O    ]")
+   print("[          ]")
+   print("[----------]")
 
 if number==4:
-    print("You rolled a four!")
-    print("[----------]")
-    print("[          ]")
-    print("[   O O    ]")
-    print("[   O O    ]")
-    print("[          ]")
-    print("[----------]")
+   print("You rolled a four!")
+   print("[----------]")
+   print("[          ]")
+   print("[   O O    ]")
+   print("[   O O    ]")
+   print("[          ]")
+   print("[----------]")
 
 if number==5:
-    print("You rolled a five!")
-    print("[---------]")
-    print("[  O   O  ]")
-    print("[    O    ]")
-    print("[  O   O  ]")
-    print("[---------]")
+   print("You rolled a five!")
+   print("[---------]")
+   print("[  O   O  ]")
+   print("[    O    ]")
+   print("[  O   O  ]")
+   print("[---------]")
 
 if number==6:
-    print("You rolled a six!")   
-    print("[---------]")
-    print("[   O O   ]")
-    print("[   O O   ]")
-    print("[   O O   ]")
-    print("[---------]")
+   print("You rolled a six!")   
+   print("[---------]")
+   print("[   O O   ]")
+   print("[   O O   ]")
+   print("[   O O   ]")
+   print("[---------]")
+
+while True:
+ import random
+
+ print("")
+ print("Press enter to roll again")
+ input()
+
+ if number==1:
+   print("You rolled a one!")
+   print("[----------]")
+   print("[          ]")
+   print("[     O    ]")
+   print("[          ]")
+   print("[----------]")
+
+ if number==2:
+   print("You rolled a two!")
+   print("[----------]")
+   print("[          ]")
+   print("[   O  O   ]")
+   print("[          ]")
+   print("[----------]")
+    
+ if number==3:
+   print("You rolled a three!")
+   print("[----------]")
+   print("[          ]")
+   print("[    O     ]")
+   print("[   O O    ]")
+   print("[          ]")
+   print("[----------]")
+
+ if number==4:
+   print("You rolled a four!")
+   print("[----------]")
+   print("[          ]")
+   print("[   O O    ]")
+   print("[   O O    ]")
+   print("[          ]")
+   print("[----------]")
+
+ if number==5:
+   print("You rolled a five!")
+   print("[---------]")
+   print("[  O   O  ]")
+   print("[    O    ]")
+   print("[  O   O  ]")
+   print("[---------]")
+
+ if number==6:
+   print("You rolled a six!")   
+   print("[---------]")
+   print("[   O O   ]")
+   print("[   O O   ]")
+   print("[   O O   ]")
+   print("[---------]")
