4fd5d0f9b2021c0558229317e830ded406339d08
Joshua Savon <jsavon@mail.valenciacollege.edu>
2016-04-27 20:16:32 -0400

    Dice roll
    Simple dice roll

---
+++ devel/diceroll.py
@@ -0,0 +1,58 @@
+import random
+
+print("Welcome to Josh's Dice rolling program!")
+print("Press enter to roll the die")
+input()
+
+number=random.randint(1,6)
+
+if number==1:
+    print("You rolled a one!")
+    print("[----------]")
+    print("[          ]")
+    print("[     O    ]")
+    print("[          ]")
+    print("[----------]")
+
+if number==2:
+    print("You rolled a two!")
+    print("[----------]")
+    print("[          ]")
+    print("[   O  O   ]")
+    print("[          ]")
+    print("[----------]")
+    
+
+if number==3:
+    print("You rolled a three!")
+    print("[----------]")
+    print("[          ]")
+    print("[    O     ]")
+    print("[   O O    ]")
+    print("[          ]")
+    print("[----------]")
+
+if number==4:
+    print("You rolled a four!")
+    print("[----------]")
+    print("[          ]")
+    print("[   O O    ]")
+    print("[   O O    ]")
+    print("[          ]")
+    print("[----------]")
+
+if number==5:
+    print("You rolled a five!")
+    print("[---------]")
+    print("[  O   O  ]")
+    print("[    O    ]")
+    print("[  O   O  ]")
+    print("[---------]")
+
+if number==6:
+    print("You rolled a six!")   
+    print("[---------]")
+    print("[   O O   ]")
+    print("[   O O   ]")
+    print("[   O O   ]")
+    print("[---------]")
